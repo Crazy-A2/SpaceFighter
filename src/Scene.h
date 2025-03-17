1cf7a25459d49fa81a0ec286ff0d13646d5c2d3f
Crazy_AI <qwe453931370@163.com>
2025-03-18 01:16:04 +0800

    更新注释

--- src/Scene.h
+++ src/Scene.h
@@ -2,6 +2,7 @@
 
 #include <SDL_events.h>
 
+// 场景接口，所有场景必须实现该接口
 struct Scene {
     Scene() = default;
     virtual ~Scene() = default; // 基类此处必须为虚析构函数 否则派生类析构无法释放正确的资源
--- src/SceneMain.h
+++ src/SceneMain.h
@@ -11,42 +11,29 @@ struct Game; // 不导入头文件 防止可能的循环引用
 
 // 定义一个名为 SceneMain 的结构体，继承自 Scene 类，并使用 final 关键字表示不允许被继承
 struct SceneMain final : Scene {
-    // 构造函数，用于初始化 SceneMain 对象
     SceneMain();
-    // 析构函数，用于清理 SceneMain 对象
     ~SceneMain();
 
-    // 重写 Scene 类的 init 方法，用于初始化场景
-    void init() override;
-    // 重写 Scene 类的 update 方法，用于更新场景状态，deltaTime 表示时间间隔
-    void update(float deltaTime) override;
-    // 重写 Scene 类的 render 方法，用于渲染场景
-    void render() override;
-    // 重写 Scene 类的 clean 方法，用于清理场景资源
-    void clean() override;
-    // 重写 Scene 类的 handleEvents 方法，用于处理事件，event 指向 SDL_Event 结构体
-    void handleEvents(SDL_Event* event) override;
-    // 处理键盘控制，deltaTime 表示时间间隔
-    void keyboardControl(float deltaTime);
-    // 处理玩家射击
-    void shootPlayer();
-    // 更新玩家投射物
-    // 参数：deltaTime - 时间增量，通常用于表示自上次更新以来的时间间隔
-    void updatePlayerProjectiles(float deltaTime);
-    // 渲染玩家投射物
-    void renderPlayerProjectiles();
+    void init() override; // 初始化场景
+    void update(float deltaTime) override; // 更新场景状态，deltaTime 表示时间间隔
+    void render() override; // 渲染场景
+    void clean() override; // 清理场景资源
+    void handleEvents(SDL_Event* event) override; // 处理事件
+    void keyboardControl(float deltaTime); // 处理键盘控制，deltaTime 表示时间间隔
+    void shootPlayer(); // 处理玩家射击
+    void updatePlayerProjectiles(float deltaTime); // 计算玩家子弹相关逻辑 deltaTime 表示时间间隔
+    void renderPlayerProjectiles(); // 渲染玩家子弹
+    void spawEnemy(); // 生成敌人
+    void updateEnemies(float deltaTime); // 更新敌人逻辑 deltaTime 表示时间间隔
+    void renderEnemies(); // 渲染敌人
 
 private:
-    // 引用 Game 对象，用于访问游戏状态和资源
-    Game& game;
-    // 玩家对象
-    Player player;
-    // 玩家投射物模板对象
-    ProjectilePlayer projectilePlayerTemplate;
-    // 存储玩家投射物对象的列表
-    std::list<ProjectilePlayer*> projectilesPlayer;
-    // 定义一个随机数生成器对象 gen，使用 Mersenne Twister 19937 算法
-    std::mt19937 gen;
-    // 定义一个均匀分布的对象 dis，用于生成浮点数，范围默认为 [0, 1)
-    std::uniform_real_distribution<float> dis;
+    Game& game; // 引用 Game 对象，用于访问游戏状态和资源
+    Player player; // 玩家对象
+    ProjectilePlayer projectilePlayerTemplate; // 定义一个玩家子弹的模板对象
+    std::list<ProjectilePlayer*> projectilesPlayer; // 存储玩家子弹对象的子弹列表
+    Enemy enemyTemplate; // 定义一个敌人的模板对象
+    std::list<Enemy*> enemies; // 存储敌人对象的列表
+    std::mt19937 gen; // 定义一个随机数生成器对象 gen，使用 Mersenne Twister 19937 算法
+    std::uniform_real_distribution<float> dis; // 定义一个均匀分布的对象 dis，用于生成浮点数
 };
